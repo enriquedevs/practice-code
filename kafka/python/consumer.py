c934cea4dd32d997e4074b8986f431521e06821b
Enrique Garcia <nitos_san@hotmail.com>
2022-06-22 10:19:20 -0500

    Updating producer consumer code for rail system

--- kafka/python/consumer.py
+++ kafka/python/consumer.py
@@ -1,6 +1,6 @@
 from kafka import KafkaConsumer
 
-topic_name = 'hello-world-topic'
+topic_name = 'train-departures-topic'
 servers = ['b-1.kafka-enroute.mx95vv.c11.kafka.us-west-2.amazonaws.com:9092',
            'b-2.kafka-enroute.mx95vv.c11.kafka.us-west-2.amazonaws.com:9092',
            'b-3.kafka-enroute.mx95vv.c11.kafka.us-west-2.amazonaws.com:9092']
--- rail-system/rail_data_kafka_ingestion.py
+++ rail-system/rail_data_kafka_ingestion.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+from http.client import RemoteDisconnected
+
 from zeep import Client
 from zeep import xsd
 from zeep.plugins import HistoryPlugin
@@ -15,7 +17,7 @@ servers = ['b-1.kafka-enroute.mx95vv.c11.kafka.us-west-2.amazonaws.com:9092',
 LDB_TOKEN = 'f3b043b1-db1a-41bc-b086-9b4f8fc39df1'
 WSDL = 'http://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2017-10-01'
 
-time_interval_in_seconds = 60
+time_interval_in_seconds = 10
 
 crs_list = ['EUS', 'LIV', 'GLC', 'BHM', 'WFJ', 'CRE']
 crs_list_size = len(crs_list)
@@ -25,7 +27,13 @@ if LDB_TOKEN == '':
 
 history = HistoryPlugin()
 
-client = Client(wsdl=WSDL, plugins=[history])
+def initiliaze_client():
+    client = Client(wsdl=WSDL, plugins=[history])
+    return client
+
+
+client = initiliaze_client()
+
 
 header = xsd.Element(
     '{http://thalesgroup.com/RTTI/2013-11-28/Token/types}AccessToken',
@@ -46,7 +54,14 @@ print(f"========= Producer process running, Starting sending every {time_interva
 
 
 def retrieve_train_departure_data(crs):
-    res = client.service.GetDepartureBoard(numRows=3, crs=crs, _soapheaders=[header_value])
+    global client
+    try:
+        res = client.service.GetDepartureBoard(numRows=3, crs=crs, _soapheaders=[header_value])
+    except RemoteDisconnected as excpetion:
+        print(excpetion)
+        print('-------Initialize again the client and retrieving data------')
+        client = initiliaze_client()
+        res = client.service.GetDepartureBoard(numRows=3, crs=crs, _soapheaders=[header_value])
 
     print(f"------Trains at {res.locationName} ({crs})------")
     print("===============================================================================")
